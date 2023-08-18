e047377dbfba09b4f245804a49db2a667d4b51dd
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-19 01:46:36 +0800

    test: change test case names

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SmartParkingBoyTest {
 
     @Test
-    void should_park_to_parking_lot_with_more_parking_spaces_when_park_given_a_standard_parking_boy_and_two_parking_lots_with_one_have_more_parking_spaces_and_a_car() {
+    void should_park_to_parking_lot_with_more_parking_space_when_park_given_a_smart_parking_boy_and_two_parking_lots_with_one_have_more_parking_space_and_a_car() {
         // Given
         ParkingLot parkingLotWithLessSpace = new ParkingLot(5);
         ParkingLot parkingLotWithMoreSpace = new ParkingLot(10);
@@ -29,7 +29,7 @@ class SmartParkingBoyTest {
     }
 
     @Test
-    void should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_with_equal_parking_spaces_and_a_car() {
+    void should_park_to_first_parking_lot_when_park_given_a_smart_parking_boy_and_two_parking_lots_with_equal_parking_space_and_a_car() {
         // Given
         ParkingLot firstParkingLot = new ParkingLot();
         ParkingLot secondParkingLot = new ParkingLot();
