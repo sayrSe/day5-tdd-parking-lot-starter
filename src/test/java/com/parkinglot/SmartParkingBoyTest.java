3a5e0968f699c124f4ebd5d03f93a6fc28ea6a6d
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-19 00:04:52 +0800

    test: fix text case name according to given

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class SmartParkingBoyTest {
 
     @Test
-    void should_park_to_parking_lot_with_more_parking_spaces_when_park_given_a_standard_parking_boy_and_two_parking_lots_with_one_have_more_parking_spaces() {
+    void should_park_to_parking_lot_with_more_parking_spaces_when_park_given_a_standard_parking_boy_and_two_parking_lots_with_one_have_more_parking_spaces_and_a_car() {
         // Given
         ParkingLot parkingLotWithLessSpace = new ParkingLot(5);
         ParkingLot parkingLotWithMoreSpace = new ParkingLot(10);
