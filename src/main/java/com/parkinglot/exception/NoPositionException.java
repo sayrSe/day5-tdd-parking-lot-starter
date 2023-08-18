79676871a895824ea95fda351f423c397c12e866
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-18 19:01:32 +0800

    refactor: NoAvailablePositionException class name

--- src/main/java/com/parkinglot/exception/NoPositionException.java
+++
@@ -1,7 +0,0 @@
-package com.parkinglot.exception;
-
-public class NoPositionException extends RuntimeException {
-    public NoPositionException() {
-        super("No available position.");
-    }
-}
