37c9e808add5607d7a0ad5f7003cb51ba54b3266
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-18 19:16:23 +0800

    test: create test case when first parking lot full and second parking lot has available position

--- src/test/java/com/parkinglot/StandardParkingBoyTest.java
+++ src/test/java/com/parkinglot/StandardParkingBoyTest.java
@@ -25,4 +25,24 @@ class StandardParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(10, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_park_to_second_parking_lot_when_park_given_a_standard_parking_boy_and_first_parking_lot_full_and_second_has_available_position_and_a_car() {
+    	// Given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+        Car parkedCar = new Car();
+        firstParkingLot.park(parkedCar);
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        StandardParkingBoy standardParkingBoy = new StandardParkingBoy(parkingLots);
+        Car car = new Car();
+
+    	// When
+        ParkingTicket parkingTicket = standardParkingBoy.park(car);
+
+    	// Then
+        assertNotNull(parkingTicket);
+        assertEquals(0, firstParkingLot.getAvailableCapacity());
+        assertEquals(1, secondParkingLot.getAvailableCapacity());
+    }
 }
