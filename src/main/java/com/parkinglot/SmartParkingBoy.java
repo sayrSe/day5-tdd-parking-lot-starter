b10c4bc598e5876e40771cd05161c310c36ce6c0
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-19 01:12:15 +0800

    refactor: getParkingLotWithMoreSpace return Optional ParkingLot

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -4,7 +4,7 @@ import com.parkinglot.exception.NoAvailablePositionException;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 public class SmartParkingBoy extends StandardParkingBoy {
 
@@ -13,17 +13,14 @@ public class SmartParkingBoy extends StandardParkingBoy {
     }
 
     public ParkingTicket park(Car car) {
-        return getParkingLotWithMoreSpace(super.getParkingLots()).stream()
+        return getParkingLotWithMoreSpace(super.getParkingLots())
                 .filter(ParkingLot::hasAvailableCapacity)
-                .findFirst()
                 .orElseThrow(NoAvailablePositionException::new)
                 .park(car);
     }
 
-    private static List<ParkingLot> getParkingLotWithMoreSpace(List<ParkingLot> parkingLots) {
+    private static Optional<ParkingLot> getParkingLotWithMoreSpace(List<ParkingLot> parkingLots) {
         return parkingLots.stream()
-                .max(Comparator.comparing(ParkingLot::getAvailableCapacity))
-                .stream()
-                .collect(Collectors.toList());
+                .max(Comparator.comparing(ParkingLot::getAvailableCapacity));
     }
 }
