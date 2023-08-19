31075b07b1eef1f5e304e345da617da678dc8d3a
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-19 22:41:26 +0800

    test: create test case when super parking boy park on parking lots with equal available position rate

--- src/test/java/com/parkinglot/parkingboy/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/parkingboy/SuperParkingBoyTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static com.parkinglot.parkingboy.ParkingBoyTestDataFactory.buildParkingLotWithFiveCapacityAndTwoParkedCar;
-import static com.parkinglot.parkingboy.ParkingBoyTestDataFactory.buildParkingLotWithThreeCapacityAndOneParkedCar;
+import static com.parkinglot.parkingboy.ParkingBoyTestDataFactory.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SuperParkingBoyTest {
@@ -29,5 +28,21 @@ class SuperParkingBoyTest {
         assertNotNull(parkingTicket);
         assertEquals(1, parkingLotWithLargerPosRate.getAvailableCapacity());
         assertEquals(3, parkingLotWithSmallerPosRate.getAvailableCapacity());
+    }
+
+    @Test
+    void should_park_to_first_parking_lot_when_park_given_a_super_parking_boy_and_two_parking_lots_with_equal_available_position_rate() {
+        // Given
+        List<ParkingLot> parkingLots = buildTwoEmptyParkingLots();
+        SuperParkingBoy superParkingBoy = new SuperParkingBoy(parkingLots);
+        Car car = new Car();
+
+        // When
+        ParkingTicket parkingTicket = superParkingBoy.park(car);
+
+        // Then
+        assertNotNull(parkingTicket);
+        assertEquals(9, parkingLots.get(0).getAvailableCapacity());
+        assertEquals(10, parkingLots.get(1).getAvailableCapacity());
     }
 }
